bf36849636a23886688fa85f1576ac13d6de93a4
Pietro <64149083+pietrusko@users.noreply.github.com>
2024-08-18 22:48:34 -0400

    Creación app.py

---
+++ backend/app.py
@@ -0,0 +1,29 @@
+From flask import Flask, request, jsonify
+Import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/scan', methods=['POST'])
+def pscan_CodigoBarra():
+    data = request.get_json()
+    CodigoBarra = data.get('CodigoBarra')
+
+    df = pd.read.excel('Inventario base prueba.xlsx')
+
+    columna_producto = df[df["Codigo barra"] == CodigoBarra]
+
+    if columna_producto.empty:
+        return jsonify({"Error": "Producto no encontrado"}), 404
+    
+    respuesta = 
+    {
+        "Producto": columna_producto["Producto"].values[0],
+        "inventario": int(columna_producto["Inventario"].values[0]),
+        "Id": columna_producto["Id"].values[0],
+        "Codigo barra": columna_producto["Codigo barra"].values[0]
+    }
+
+    return jsonify(respuesta)
+
+if __name__ == '__main__':
+    app.run(debug=True)
